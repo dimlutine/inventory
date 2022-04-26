71587df33eec37e13507b49729cea962e0b01520
dimlu <dimlutine@gmail.com>
2022-04-26 18:19:57 -0400

    Part 4 Props, Default Props, and Prop Types

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import "./App.css";
 import Info from "./Info.js";
+import { PropTypes } from "prop-types";
 
 function App() {
   return (
     <div className="App">
-      <Info />
-      <AddItem />
+      <Info title="Inventoriorio" />
+      <AddItem text="tits" number={96} />
+      <AddItem text="bewbs" number={69} />
+      <AddItem text="knockers" number={420} />
     </div>
   );
 }
 
-function AddItem() {
+function AddItem(props) {
   return (
     <form>
       <label htmlFor="" className="for">
         Type something:{" "}
       </label>
-      <input type="text" id="text-form" />
+      <input type="text" id="text-form" value={props.value} />
+      <br />
+      <label htmlFor="" className="for">
+        {props.number}
+      </label>
     </form>
   );
 }
+
+AddItem.PropTypes = {
+  number: PropTypes.number,
+  text: PropTypes.string,
+};
 
 export default App;
