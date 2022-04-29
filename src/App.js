e7b54c4837b748d2ae32187cb5a07a38bbcc7a62
dimlu <dimlutine@gmail.com>
2022-04-29 19:59:14 -0400

    Part 6 Forms and Events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import SearchBar from "./SearchBar.js";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState({});
+
+  const updateData = (searchParams) => {
+    setData(searchParams);
+  };
+  return (
+    <div className="App">
+      <SearchBar callback={updateData} />
+      <p>Name: {"name" in data ? data["name"] : "no data to display"}</p>
+      <p>Price: {"price" in data ? data["price"] : "no data to display"}</p>
+      <p>Type: {"type" in data ? data["type"] : "no data to display"}</p>
+      <p>Brand: {"brand" in data ? data["brand"] : "no data to display"}</p>
+    </div>
+  );
 }
 
 export default App;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,1 +1,56 @@
 import { useState } from "react";
+
+function SearchBar(props) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState();
+  const [type, setType] = useState("");
+  const [brand, setBrand] = useState("");
+  const searchButtonPressed = () => {
+    props.callback({
+      name: name,
+      price: price,
+      type: type,
+      brand: brand,
+    });
+  };
+  return (
+    <div>
+      <h2>Search for an item</h2>
+      <form action="">
+        <label htmlFor="name-field">Name:</label>
+        <input
+          type="text"
+          id="name-field"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="price-field">Price:</label>
+        <input
+          type="number"
+          id="price-field"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <label htmlFor="type-field">Type:</label>
+        <input
+          type="text"
+          id="type-field"
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+        />
+        <label htmlFor="brand-field">Brand:</label>
+        <input
+          type="text"
+          id="brand-field"
+          value={brand}
+          onChange={(e) => setBrand(e.target.value)}
+        />
+        <button type="button" onClick={searchButtonPressed}>
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
