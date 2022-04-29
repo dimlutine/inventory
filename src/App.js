a4c6d9c8aedac0500e026142d459feccba769f0f
dimlu <dimlutine@gmail.com>
2022-04-28 20:36:32 -0400

    Part 5.5 Global Snippets

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import "./App.css";
-import Info from "./Info.js";
-import { PropTypes } from "prop-types";
-import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Info title="Inventoriorio" />
-      <ButtonState />
-    </div>
-  );
+  return <div className="App"></div>;
 }
-
-function ButtonState() {
-  const [title, setTitle] = useState("");
-  const [count, setCount] = useState(0);
-
-  const updateTitleClicked = () => {
-    setTitle("Tittle");
-  };
-
-  const updateCountClicked = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <Data title={title} count={count} />
-      <button onClick={updateTitleClicked}>Update Title </button>
-      <button onClick={updateCountClicked}>Update Count </button>
-    </div>
-  );
-}
-
-const Data = (props) => {
-  return (
-    <div>
-      <p>Title: {props.title}</p>
-      <p>Count: {props.count}</p>
-    </div>
-  );
-};
 
 export default App;
